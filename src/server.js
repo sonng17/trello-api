09b6e5257188dcabd4769fdfe0bbb8047eeb0f50
sonng17 <ndson1703@gmail.com>
2024-01-09 16:03:39 +0700

    express routes

--- src/server.js
+++ src/server.js
@@ -3,14 +3,12 @@ import express from "express"
 import exitHook from "async-exit-hook"
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import {env} from '~/config/environment'
+import {APIs_V1} from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express();
 
-  app.get("/", async (req, res) => {
-    console.log(process.env);
-    res.end("<h1>Hello World!</h1><hr>");
-  });
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
