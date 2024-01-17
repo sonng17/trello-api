a8e41c07fd7c923bcbe217691a06b4c0b6b2ea58
sonng17 <ndson1703@gmail.com>
2024-01-17 23:43:45 +0700

    fix

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { ObjectId } from "mongodb";
 import { GET_DB } from "~/config/mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
@@ -18,11 +19,20 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+
 const createNew = async (data) => {
   try {
-    // const createdBoard = await GET_DB().collection( BOARD_COLLECTION_NAME).insertOne(data);
-    // return createdBoard
-    return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data);
+    const validData = await validateBeforeCreate(data);
+    console.log(validData);
+    const createdBoard = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .insertOne(validData);
+    return createdBoard;
   } catch (error) {
     throw new Error(error);
   }
@@ -30,9 +40,12 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id,
-    });
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id),
+      });
+    return result;
   } catch (error) {
     throw new Error(error);
   }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -10,22 +10,18 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title),
     };
 
-    // Trả kết quả về trong Service luôn phải có return
-    return newBoard
-
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong db
-    //...
+    const createdBoard = await boardModel.createNew(newBoard);
+    // console.log(createdBoard);
 
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay k)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
+    // console.log(getNewBoard);
+    
     // Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án ...vv
 
-    // const createdBoard = await boardModel.createNew(newBoard);
-    // console.log(createdBoard);
-    // const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
-    // console.log(getNewBoard);
-
-    // //tra ket qua ve, trong service luon phai co return
-    // return getNewBoard;
-
+    // Trả kết quả về trong Service luôn phải có return
+    return getNewBoard
   } catch (error) {
     throw error;
   }
